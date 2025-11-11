e25a7daf72194d4a2c1a3ed2d68c71a23560b4d5
Kritsiri <kritsiri-nd@gmail.com>
2025-11-11 18:51:36 +0700

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<!-- About.vue -->
+<template>
+    <div class="p-4">
+        <nav class="bg-gray-800 p-4 text-white flex justify-between">
+            <div class="text-lg font-semibold">TravelApp</div>
+            <div>
+                <router-link to="/" class="mr-4 hover:underline">Home</router-link>
+                <router-link to="/about" class="hover:underline">About</router-link>
+            </div>
+        </nav>
+        <h1 class="text-2xl font-bold text-green-600">About Page</h1>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="p-4">
+        <nav class="bg-gray-800 p-4 text-white flex justify-between">
+            <div class="text-lg font-semibold">TravelApp</div>
+            <div>
+                <router-link to="/" class="mr-4 hover:underline">Home</router-link>
+                <router-link to="/about" class="hover:underline">About</router-link>
+            </div>
+        </nav>
+        <h1 class="text-3xl font-bold text-blue-600">Home Page</h1>
+    </div>
+</template>
